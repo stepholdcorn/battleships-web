75e70c9631a2bbefbe8e723d08072641a36eca68
Josh Bebbington <josh@focalwebsites.com>
2015-01-06 11:43:08 +0000

    Started player registration logic and view

--- features/starting_a_game.feature
+++ features/starting_a_game.feature
@@ -6,4 +6,4 @@ Feature: Starting the game
   Scenario: Registering
     Given I am on the homepage
     When I follow "New Game"
-    Then I should see "What's your name?"
+    Then I should see "Welcome player 1"
--- lib/battleships.rb
+++ lib/battleships.rb
@@ -9,6 +9,11 @@ set :views, Proc.new { File.join(root, '..', "views") }
     erb :index
   end
 
+  get '/register' do
+    'What\'s your name?'
+    erb :register
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
