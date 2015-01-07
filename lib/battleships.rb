94cc4fde8b31092250df2518c2eaf56066a4a97a
Josh Bebbington <josh@focalwebsites.com>
2015-01-07 19:00:32 +0000

    Started implementing sessions to persist player data

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -9,6 +9,7 @@ require_relative 'cell'
 class BattleShips < Sinatra::Base
 
 set :views, Proc.new { File.join(root, '..', "views") }
+enable :sessions
 
 	def initialize
   		@game = Game.new
@@ -33,6 +34,7 @@ set :views, Proc.new { File.join(root, '..', "views") }
     else
       @player = Player.new
       @player.name = @name
+      session[:player] = @player.object_id
       @game.add_player(@player)
       erb :registered
     end
@@ -43,12 +45,13 @@ set :views, Proc.new { File.join(root, '..', "views") }
   end
 
   post '/ship_placement' do
-    @aircraft_carrier = @board.place(Ship.aircraft_carrier, params[:ac_coord].to_sym, params[:ac_orientation].to_sym)
-    @battleship = @board.place(Ship.battleship, params[:bs_coord].to_sym, params[:bs_orientation].to_sym)
-    @destroyer = @board.place(Ship.destroyer, params[:d_coord].to_sym, params[:d_orientation].to_sym)
-    @submarine = @board.place(Ship.submarine, params[:s_coord].to_sym, params[:s_orientation].to_sym)
-    @patrol_boat = @board.place(Ship.patrol_boat, params[:pb_coord].to_sym, params[:pb_orientation].to_sym)
-    erb :game_page
+    @player_session = session[:player]
+    @aircraft_carrier = @player.board.place(Ship.aircraft_carrier, params[:ac_coord].to_sym, params[:ac_orientation].to_sym)
+    @battleship = @player.board.place(Ship.battleship, params[:bs_coord].to_sym, params[:bs_orientation].to_sym)
+    @destroyer = @player.board.place(Ship.destroyer, params[:d_coord].to_sym, params[:d_orientation].to_sym)
+    @submarine = @player.board.place(Ship.submarine, params[:s_coord].to_sym, params[:s_orientation].to_sym)
+    @patrol_boat = @player.board.place(Ship.patrol_boat, params[:pb_coord].to_sym, params[:pb_orientation].to_sym)
+    redirect '/game_page'
   end
 
   get '/game_page' do
